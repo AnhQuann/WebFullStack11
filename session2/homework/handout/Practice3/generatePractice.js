27e69742ca4bdab5b39ff7736bc93ada0afa7a99
AnhQuann <anhquan19051905@gmail.com>
2018-03-11 00:14:08 +0700

    .

--- session2/homework/handout/Practice3/generatePractice.js
+++ session2/homework/handout/Practice3/generatePractice.js
@@ -1,13 +1,35 @@
 'use strict'
 
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random()*(b-a+1)) + a;
+}
+
 function generate(testLengthArray){
-  return Array.from({length : testLengthArray.length})
-    .map(item => ({
-      input: Array.from({length: item}).map(item => []),
-      target: 0,
-      output: -1
+  var result = [];
+  if (testLengthArray < 4) {
+    testLengthArray.forEach((item, index) => {
+      var object = {};
+      object.input = [];
+      for (let i = 0; i < item; i++) {
+        object.input.push(getRandomNumber(-10000, 10000));
+      }
+      object.target = object.input(getRandomNumber(0, item));
+      object.output = object.input.indexOf(object, target);
+      result.push(object);
     })
-  ); // Remove this line and change to your own algorithm
+  } else {
+    testLengthArray.forEach((item, index) => {
+      var object = {};
+      object.input = [];
+      for (let i = 0; i < item; i++) {
+        object.input.push(getRandomNumber(-10000, 10000));
+      }
+      object.target = object.input(getRandomNumber(0, item));
+      object.output = object.input.indexOf(object, target);
+      result.push(object);
+    });
+  }
+  return result;
 }
 
 module.exports = generate
